39c46c5b29f14755b2a07a28e59df7c1a4262d04
Aadarsh Shaheb Singh <aadarshaheb.singh@gmail.com>
2025-05-15 17:02:32 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import { div } from "framer-motion/client";
-import Image from "next/image";
-import SymptomPage from "../../_Components/SymptomPage";
 import Navbar from "../../_Components/Navbar";
 import HeroSection from "../../_Components/HeroSection";
 import FeaturesSection from "../../_Components/FeatureSection";
 import Footer from "../../_Components/Footer";
-import PrescriptoSathi from "../../_Components/PrescriptoSaathi";
-import PharmaSahayak from "../../_Components/PharmaSahayak";
-
-import Feature from "../../_Components/Feature";
 
 
 export default function Home() {
@@ -18,28 +12,6 @@ export default function Home() {
       <HeroSection/>
       <FeaturesSection/>
       <Footer/>
-
-      {/* <Nearby/> */}
-      {/* <Ne/> */}
-      {/* <AboutHelix/> */}
-
-      {/* /<SymptomPage/> */}
-
-      {/* <Prayog/> */}
-      {/* <AuthPages /> */}
-      {/* <PharmaSahayak/> */}
-
-      {/* <ImageSlider/> */}
-      {/* <PrescriptoSathi/> */}
-      {/* <Feature /> */}
-
-
-      {/* <Allergies></Allergies> */}
-      {/* <Navbar3/>
-      <HeroSection3/>
-      <Slider/>
-      <OurFeatures/>
-      <HomePage3/> */}
     </div>
   );
 }
